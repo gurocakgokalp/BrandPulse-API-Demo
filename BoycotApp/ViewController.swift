56b1908babe01a1b471c601e8977562978f1d239
gokalp <ggurocak58@gmail.com>
2025-10-23 18:49:16 +0300

    Initial Commit

---
+++ BoycotApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoycotApp
+//
+//  Created by Gökalp Gürocak on 23.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BoycotAppTests/BoycotAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoycotAppTests.swift
+//  BoycotAppTests
+//
+//  Created by Gökalp Gürocak on 23.10.2025.
+//
+
+import Testing
+@testable import BoycotApp
+
+struct BoycotAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
